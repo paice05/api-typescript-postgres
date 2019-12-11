11eac2fe3b024a35f1e6d15d1868454dfd86d827
Matheus Paice <matheuspaice@mandeumzap.com.br>
2019-12-11 13:54:02 -0300

    add method update and destroy

--- src/controller/BaseController.js
+++ src/controller/BaseController.js
@@ -17,6 +17,8 @@ class BaseController {
 
     const response = await this.model.findByPk(id);
 
+    if (!response) return res.json({ message: "Record not found" });
+
     return res.json(response);
   }
 
@@ -26,12 +28,43 @@ class BaseController {
     return res.json(response);
   }
 
+  async update(req, res) {
+    const { id } = req.params;
+    const { body } = req;
+
+    const isRecord = await this.model.findByPk(id);
+
+    if (!isRecord) return res.json({ message: "record not found!" });
+
+    const [number, response] = await this.model.update(
+      { ...body },
+      { where: { id } },
+      { returning: true }
+    );
+
+    return res.json({ number, response });
+  }
+
+  async destroy(req, res) {
+    const { id } = req.params;
+
+    const isRecord = await this.model.findByPk(id);
+
+    if (!isRecord) return res.json({ message: "record not found!" });
+
+    await this.model.destroy({ where: { id } });
+
+    return res.status(200).json({ message: "OK" });
+  }
+
   routes() {
     const route = Router();
 
     route.get(this.path, this.index.bind(this));
     route.get(`${this.path}/:id`, this.show.bind(this));
     route.post(this.path, this.store.bind(this));
+    route.put(`${this.path}/:id`, this.update.bind(this));
+    route.delete(`${this.path}/:id`, this.destroy.bind(this));
 
     return route;
   }
